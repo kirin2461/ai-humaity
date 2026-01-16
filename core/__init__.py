f471c31fd59f43b3ce449d12ef6f256eeb34a357
kirin2461 <83828028+kirin2461@users.noreply.github.com>
2026-01-16 05:02:43 +0300

    fix: add AutonomousLife and complete exports in core/__init__.py

--- core/__init__.py
+++ core/__init__.py
@@ -1,5 +1,13 @@
-from .emotion_engine import EmotionEngine, EmotionType
+from .emotion_engine import EmotionEngine, EmotionType, PADState
 from .cognitive_cycle import CognitiveCycle
-from .skill_system import SkillSystem
-from .safety_system import SafetySystem
-__all__ = ['EmotionEngine', 'EmotionType', 'CognitiveCycle', 'SkillSystem', 'SafetySystem']
+from .skill_system import SkillSystem, Skill, SkillLevel
+from .safety_system import SafetySystem, SafetyMode
+from .autonomous_life import AutonomousLife
+
+__all__ = [
+    'EmotionEngine', 'EmotionType', 'PADState',
+    'CognitiveCycle',
+    'SkillSystem', 'Skill', 'SkillLevel',
+    'SafetySystem', 'SafetyMode',
+    'AutonomousLife',
+]
